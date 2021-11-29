543c020a143177ae451e7cbf1c9338122ccb5714
KikiroCID <95206960+KikiroCID@users.noreply.github.com>
2021-11-29 15:40:58 +0900

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,6 @@
+# If you already install g++ or build-essential, use this
+clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot -w #Build source code into executable
+echo "⚠This Script Not For Sell, Beacause Its Free⚠"
+echo "Tool Successfully Builded,"
+echo "You Can Start Creating ID By Typing ./gtbot"
+echo "©Subscribe ZaqxKun GT"
---
+++ customing.h
@@ -0,0 +1,18 @@
+#include <string>
+#include "corefunc.h"
+#define byte uint8_t
+#define BYTE uint8_t
+
+
+//There Was Customing Area
+
+//Customing GrowID
+string Growid_acc = "ZaqxKunH";
+
+//Customing Password [Premium Only!]
+
+//Customing Gmail [Premium+ Only!]
+
+//Customing Start From (Account) [Premium Only!]
+
+//Customing Start From (Gmail) [Premium+ Only!]
